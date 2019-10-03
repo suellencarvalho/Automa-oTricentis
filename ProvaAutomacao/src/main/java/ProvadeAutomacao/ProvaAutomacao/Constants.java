c960347b2eca194b483ef956a370a949b48afeb0
suellen carvalho <suellenncontato@gmail.com>
2019-10-03 07:27:34 -0300

    Correção bug data

--- ProvaAutomacao/src/main/java/ProvadeAutomacao/ProvaAutomacao/Constants.java
+++
@@ -1,9 +0,0 @@
-package ProvadeAutomacao.ProvaAutomacao;
-
-import java.io.File;
-
-public class Constants {
-	
-	public static final String PATH_IMAGEM = new File("").getAbsolutePath() + "src\\main\\Resource\\download.png";
-
-}
