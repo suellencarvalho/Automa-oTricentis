7ff6c940004f476c869d327ccb34f2946915ee1e
suellen carvalho <suellenncontato@gmail.com>
2019-10-03 07:52:25 -0300

    Correção Bug Campo de data

---
+++ ProvaAutomacao/ProvaAutomacao/src/main/java/ProvadeAutomacao/ProvaAutomacao/App.java
@@ -0,0 +1,13 @@
+package ProvadeAutomacao.ProvaAutomacao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
